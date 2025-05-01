d7040f06c5385ed3b283bb21d7a04f00ed371a47
Nico Wagner <info@ni-wa.de>
2025-05-01 22:56:08 +0200

    feat: remove axios as dependency

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,40 @@
-import {Axios} from "./lib/axios";
-
-
 export class SolarLogAPI {
     private readonly host: string;
-    private axios: Axios;
     private WRs: number[];
 
     constructor(host: string, WRs: number[]) {
         this.host = host;
         this.WRs = WRs;
-        this.axios = new Axios(this.host);
     }
 
-    getStatusAndProduction(): Promise<{ status: { [index: string]: string }, production: { [index: string]: string } }> {
+    getStatusAndProduction(): Promise<{
+        status: { [index: string]: string },
+        production: { [index: string]: string }
+    }> {
         return new Promise((resolve, reject) => {
             const res = {
                 status: {},
                 production: {}
             };
-            this.axios.fetch([608, 782]).then(data => {
-                this.WRs.forEach(number => {
-                    res.status[number] = data.data["608"][number.toString()];
-                    res.production[number] = data.data["782"][number.toString()];
-                });
-                resolve(res);
-            }).catch(reject);
+            const body = {
+                //WR status
+                "608": null,
+                //WATT generation
+                "782": null,
+            }
+            fetch(`http://${this.host}/getjp`, {
+                method: "POST",
+                body: JSON.stringify(body),
+                signal: AbortSignal.timeout(10_000)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    this.WRs.forEach(number => {
+                        res.status[number] = data["608"][number.toString()];
+                        res.production[number] = data["782"][number.toString()];
+                    });
+                    resolve(res);
+                }).catch(reject);
         });
     }
 }
--- types/types.d.ts
+++ types/types.d.ts
@@ -1,19 +1,13 @@
-export declare class Axios {
-    private readonly host;
-    private instance;
-
-    constructor(host: string);
-
-    fetch(keys: number[]): Promise<import("axios").AxiosResponse<any, any>>;
-}
-
-
 export declare class SolarLogAPI {
     private readonly host;
-    private axios;
     private WRs;
-
     constructor(host: string, WRs: number[]);
-
-    getStatusAndProduction(): Promise<{ status: { [index: string]: string }, production: { [index: string]: string } }>;
+    getStatusAndProduction(): Promise<{
+        status: {
+            [index: string]: string;
+        };
+        production: {
+            [index: string]: string;
+        };
+    }>;
 }
